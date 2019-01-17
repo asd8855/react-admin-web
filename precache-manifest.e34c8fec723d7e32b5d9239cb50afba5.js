1b5efd2cc0557f6e6039fd4753b73b1bae49777a
libo <asd13148866@sina.com>
2019-01-17 17:56:32 +0800

    init

---
+++ precache-manifest.e34c8fec723d7e32b5d9239cb50afba5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "39a4dd5f7147afaa22d9",
+    "url": "/react-admin-web/build/static/js/0.39a4dd5f.chunk.js"
+  },
+  {
+    "revision": "dc80837828f766364752",
+    "url": "/react-admin-web/build/static/js/1.dc808378.chunk.js"
+  },
+  {
+    "revision": "e1505a70ff51eccbc03b",
+    "url": "/react-admin-web/build/static/js/2.e1505a70.chunk.js"
+  },
+  {
+    "revision": "d63b6b578bce9ab0fae6",
+    "url": "/react-admin-web/build/static/js/3.d63b6b57.chunk.js"
+  },
+  {
+    "revision": "63392576ccad3e32d2b4",
+    "url": "/react-admin-web/build/static/js/4.63392576.chunk.js"
+  },
+  {
+    "revision": "236ff29bdf69708f686f",
+    "url": "/react-admin-web/build/static/css/main.2afce7bf.chunk.css"
+  },
+  {
+    "revision": "236ff29bdf69708f686f",
+    "url": "/react-admin-web/build/static/js/main.236ff29b.chunk.js"
+  },
+  {
+    "revision": "5afa71a2bc418e138a43",
+    "url": "/react-admin-web/build/static/css/6.437ed870.chunk.css"
+  },
+  {
+    "revision": "5afa71a2bc418e138a43",
+    "url": "/react-admin-web/build/static/js/6.5afa71a2.chunk.js"
+  },
+  {
+    "revision": "d6b7c86b0178450f8c9d",
+    "url": "/react-admin-web/build/static/js/runtime~main.d6b7c86b.js"
+  },
+  {
+    "revision": "d2f69a92faa6fe990d2e613c358be705",
+    "url": "/react-admin-web/build/static/media/element-icons.d2f69a92.woff"
+  },
+  {
+    "revision": "b02bdc1b846fd65473922f5f62832108",
+    "url": "/react-admin-web/build/static/media/element-icons.b02bdc1b.ttf"
+  },
+  {
+    "revision": "ce25bfc981f9841c6e5e698af518ef4e",
+    "url": "/react-admin-web/build/index.html"
+  }
+];
